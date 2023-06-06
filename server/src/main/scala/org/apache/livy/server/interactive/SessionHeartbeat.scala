87598b68f86263a2e5bbbfde216e9669a6c3923b
Shreyas Ghodake <66561670+Shreyasghodake@users.noreply.github.com>
2023-06-06 14:37:33 +0530

    Update SessionHeartbeat.scala

--- server/src/main/scala/org/apache/livy/server/interactive/SessionHeartbeat.scala
+++ server/src/main/scala/org/apache/livy/server/interactive/SessionHeartbeat.scala
@@ -63,6 +63,12 @@ trait SessionHeartbeatNotifier[S <: Session with SessionHeartbeat, R <: Recovery
     }
   }
 
+   abstract override protected def withNewUnprotectedSession(fn: (S => Any)): Any = {
+    super.withUnprotectedSession { s =>
+      fn(s)
+    }
+  }
+   
   abstract override protected def withViewAccessSession(fn: (S => Any)): Any = {
     super.withViewAccessSession { s =>
       s.heartbeat()
