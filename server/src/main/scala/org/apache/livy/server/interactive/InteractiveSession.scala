9b09f2480ea004b7b284a5b89126ae21eac18583
shreyasghodake <shreyasghodake2@gmail.com>
2023-07-21 14:55:09 +0530

    recovery

--- server/src/main/scala/org/apache/livy/server/interactive/InteractiveSession.scala
+++ server/src/main/scala/org/apache/livy/server/interactive/InteractiveSession.scala
@@ -677,7 +677,7 @@ class InteractiveSession(
       try
       {
         // if ( newState == SparkApp.State.FINISHED || newState == SparkApp.State.FAILED) {
-        sessionStore.saveStatement(RECOVERY_SESSION_TYPE, recoveryStatement, 0)
+        // sessionStore.saveStatement(RECOVERY_SESSION_TYPE, recoveryStatement, 0)
         // }
       }
       catch
