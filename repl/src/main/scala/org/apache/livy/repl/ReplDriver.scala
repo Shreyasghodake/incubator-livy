35dbff5eac74f18b151f4dd6a079c7eaa2eaeb8a
shreyasghodake <shreyasghodake2@gmail.com>
2023-07-24 15:16:14 +0530

    recovery

--- repl/src/main/scala/org/apache/livy/repl/ReplDriver.scala
+++ repl/src/main/scala/org/apache/livy/repl/ReplDriver.scala
@@ -33,7 +33,7 @@ import org.apache.livy.sessions._
 
 class ReplDriver(conf: SparkConf, livyConf: RSCConf)
   extends RSCDriver(conf, livyConf)
-    with Logging {
+  with Logging {
 
   private[repl] var session: Session = _
 
@@ -74,11 +74,6 @@ class ReplDriver(conf: SparkConf, livyConf: RSCConf)
   def handle(ctx: ChannelHandlerContext, msg: BaseProtocol.GetReplJobResults): ReplJobResults = {
     val statements = if (msg.allResults) {
       session.statements.values.toArray
-      statements.foreach { s =>
-        s.updateProgress(session.progressOfStatement(s.id))
-      }
-
-      new ReplJobResults(statements.sortBy(_.id))
     } else {
       assert(msg.from != null)
       assert(msg.size != null)
@@ -89,27 +84,13 @@ class ReplDriver(conf: SparkConf, livyConf: RSCConf)
           val until = msg.from + msg.size
           session.statements.filterKeys(id => id >= msg.from && id < until).values.toArray
         }
-        statements.foreach { s =>
-        s.updateProgress(session.progressOfStatement(s.id))
       }
-      new ReplJobResults(statements.sortBy(_.id))
-      }
-      catch {
-        case _: Exception =>
-          warn(s"Exception")
-      }
-      statements.foreach { s =>
-        s.updateProgress(session.progressOfStatement(s.id))
-      }
-      new ReplJobResults(statements.sortBy(_.id))
     }
-
     // Update progress of statements when queried
-    // statements.foreach { s =>
-    //   s.updateProgress(session.progressOfStatement(s.id))
-    // }
-
-    // new ReplJobResults(statements.sortBy(_.id))
+    statements.foreach { s =>
+      s.updateProgress(session.progressOfStatement(s.id))
+    }
+    new ReplJobResults(statements.sortBy(_.id))
   }
 
   override protected def createWrapper(msg: BaseProtocol.BypassJobRequest): BypassJobWrapper = {
